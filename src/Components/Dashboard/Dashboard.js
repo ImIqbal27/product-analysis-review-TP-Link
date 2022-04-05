100a241f23ba7943e20337349c0f447846a9ba25
unknown <iamhasan9501@gmail.com>
2022-04-05 21:37:02 +0600

    chart added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Barchart from '../BarChart/Barchart';
+import MyComposedChart from '../ComposedChart/MyComposedChart';
+import MyChart from '../MyChart/MyChart';
+import MyPieChart from '../MyPieChart/MyPieChart';
 
 const Dashboard = () => {
     return (
         <div>
             <h2>dashboard</h2>
+            <MyChart> </MyChart>
+            <MyPieChart></MyPieChart>
+            <Barchart></Barchart>
+            <MyComposedChart></MyComposedChart>
         </div>
     );
 };
